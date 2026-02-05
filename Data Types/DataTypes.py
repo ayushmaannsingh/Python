9e84f6eb01b8e98470aaadb2f4836b22f5894d6d
ayushmaannsingh <sa776857@gmail.com>
2026-02-05 22:21:16 +0530

    added input

--- Data Types/DataTypes.py
+++ Data Types/DataTypes.py
@@ -3,3 +3,26 @@
 # float = 10.00
 # Boolean = True , False
 # None = No value enter
+
+
+# -- INPUT in Python --
+# name = input ("enter your name: ")
+# print("you entered", name)
+
+# int("5")
+# Val = int (input("enter some value: "))
+# print(type(Val), Val)
+
+# name = input("enter name: ")
+# age = int(input("enter age: "))
+# marks = float(input("enter marks: "))
+
+# print("welcome", name)
+# print("age =", age)
+# print("marks=", marks)
+
+val1 = int (input ("enter no. 1 ="))
+val2 = int (input ("enter no. 2 ="))
+
+print("sum =", val1 + val2)
+

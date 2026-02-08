9d316f71319e547e1837dab44ec6de2dd05e0d55
ayushmaannsingh <sa776857@gmail.com>
2026-02-08 19:49:36 +0530

    Added practice question

---
+++ Conditional Statement/Question.py
@@ -0,0 +1,30 @@
+# W A P to check if a number entered by the user is odd or even.
+
+# num = int(input("enter number: "))
+
+# if(num % 2 == 0):
+#     print("EVEN")
+# else:
+#     print("ODD")
+
+# W A P to find the greatest of 3 numbers entered by the user.
+
+# a = int(input("enter first number: "))
+# b = int(input("enter second number: "))
+# c = int(input("enter third number: "))
+
+# if(a >= b and a >= c):
+#     print("first number is largest", a)
+# elif(b >= c):
+#     print("second number is largest", b)
+# else:
+#     print("third is largest", c)
+
+
+ # W A P to check if a number is multiple of 7 or not.
+num = int(input("enter number: "))
+
+if(num % 7 == 0):
+    print("multiple of 7")
+else:
+    print("not a multiple")

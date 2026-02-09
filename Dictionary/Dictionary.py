0d65e792111d5f381bae6d1aa1ef62160737fc9d
ayushmaannsingh <sa776857@gmail.com>
2026-02-10 00:11:53 +0530

    Dictionary

--- Dictionary/Dictionary.py
+++ Dictionary/Dictionary.py
@@ -1,6 +1,35 @@
-dict = {
-    "name" : "Ayush",
-    "cgpa" : 8.5,
-    "marks" : [90,80,85],
+# dict = {
+#     "name" : "Ayush",
+#     "cgpa" : 8.5,
+#     "marks" : [90,80,85],
+# }
+# print(dict)
+
+# info = {
+#     "name" : "AYush Singh",
+#     "Subjects" : ["python", "C," "Java",],
+#     "topics" : ("dict", "set"),
+#     "age" : 35,
+#     "is_adult" : True,
+#     "marks" : 94.4
+# }
+
+# info["age"] = 20
+# print(info)
+
+# null_dict = {}
+# null_dict["middlename"] = "Kumar"
+# print(null_dict)
+
+# Nested Dictionary
+
+student = {
+    "name" : "Ayush Singh",
+    "subjects" : {
+        "Maths" : 94,
+        "Chem" : 97,
+        "Phy" : 95
+    }
 }
-print(dict)
+
+print(student["subjects"]["Maths"])

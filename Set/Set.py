e3a443ab49469ceaf6e7269f8419bdbcafb857ed
ayushmaannsingh <sa776857@gmail.com>
2026-02-10 00:12:20 +0530

    Set

---
+++ Set/Set.py
@@ -0,0 +1,18 @@
+collection = {1,2,2,3,4,4, "Ayush", "Singh", "Singh"}
+
+print(collection)
+print(type(collection))
+
+# empty set synatx
+
+ayush = set()
+
+print(type(ayush))
+
+Singh = set()
+Singh.add(1)
+Singh.add(2)
+Singh.add("Ayush Singh")
+Singh.add((1,2,3))
+
+print(Singh)

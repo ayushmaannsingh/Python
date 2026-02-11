ad3546b769a4494f6e9b6a279a05243ffaf25578
ayushmaannsingh <sa776857@gmail.com>
2026-02-11 20:37:39 +0530

    while loop

--- Loops/Loops.py
+++ Loops/Loops.py
@@ -1,21 +1,29 @@
-count = 1
-while count <= 5:
-    print("Ayush")
-    count +=1;
+# count = 1
+# while count <= 5:
+#     print("Ayush")
+#     count +=1;
 
 
+# i = 1
+# while i<=5:
+#     print("Singh", i)
+#     i+=1;
+# print(i)
+
+# num = 1
+# while num<=5:
+#     print(num)
+#     num +=1
+
+# number = 5
+# while number >=1:
+#     print(number)
+#     number -=1
+
+# Print the multiplication table of a number n.
+
+n = int(input("enter number: "))
 i = 1
-while i<=5:
-    print("Singh", i)
-    i+=1;
-print(i)
-
-num = 1
-while num<=5:
-    print(num)
-    num +=1
-
-number = 5
-while number >=1:
-    print(number)
-    number -=1
+while i<=10:
+    print(n*i)
+    i+=1

99b23bffc82afd6029311925dfcd6f77b3e63d87
ayushmaannsingh <sa776857@gmail.com>
2026-02-11 23:23:49 +0530

    Loops

---
+++ Loops/Break & Continue.py
@@ -0,0 +1,20 @@
+# Break
+
+# i = 1
+# while i<=5:
+#     print(i)
+#     if(i==3):
+#         break
+#     i +=1
+
+#print("end of loop")
+
+# Continue
+
+i = 0
+while i <= 5:
+    if(i == 3):
+     i += 1
+     continue
+    print(i)
+    i += 1

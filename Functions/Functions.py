b2a9f742f7cc886960f699c63523ae57091850cc
ayushmaannsingh <sa776857@gmail.com>
2026-02-13 00:04:17 +0530

    functio

---
+++ Functions/Functions.py
@@ -0,0 +1,20 @@
+# def calcSum(a, b):
+#     return a + b
+
+# sum = calcSum(5, 10)
+# print(sum)
+
+# def printHello():
+#     print("Ayush")
+
+# printHello()
+
+# average of 3 numbers
+
+def calcAvg(a, b, c):
+    sum = a + b + c
+    avg = sum / 3
+    print(avg)
+    return avg
+
+calcAvg(94, 85, 75)

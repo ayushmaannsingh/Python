545307ffc54f58e290c0405a1a2a07585761dbe9
ayushmaannsingh <sa776857@gmail.com>
2026-02-12 23:23:00 +0530

    Loops

--- Loops/Range.py
+++ Loops/Range.py
@@ -3,14 +3,22 @@
 # for i in seq:
 #     print(i) 
 
-for i in range(10): # range(stop)
-    print(i)
+# for i in range(10): # range(stop)
+#     print(i)
 
-for i in range(2, 10): # range(start, stop)
-    print(i)
+# for i in range(2, 10): # range(start, stop)
+#     print(i)
 
-for i in range(2, 10, 2): # range(start, stop, step)
-    print(i)
+# for i in range(2, 10, 2): # range(start, stop, step)
+#     print(i)
 
-for i in range(2,20,2): # even number print
-    print(i)
+# for i in range(2,20,2): # even number print
+#     print(i)
+
+
+# Multiplication 
+
+n = int(input("enter number : "))
+
+for i in range(1, 11):
+    print(n * i) 

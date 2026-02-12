d8ffae4ab95a70ab7a34e9d42364c49d23b7ea05
ayushmaannsingh <sa776857@gmail.com>
2026-02-12 23:06:57 +0530

    Loops

--- Loops/Range.py
+++ Loops/Range.py
@@ -1,4 +1,16 @@
-seq = range(10)
+# seq = range(10)
 
-for i in seq:
+# for i in seq:
+#     print(i) 
+
+for i in range(10): # range(stop)
+    print(i)
+
+for i in range(2, 10): # range(start, stop)
+    print(i)
+
+for i in range(2, 10, 2): # range(start, stop, step)
+    print(i)
+
+for i in range(2,20,2): # even number print
     print(i)

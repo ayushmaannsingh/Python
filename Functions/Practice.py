6bc8aac7a2aa872002623cbe1fb1f59dc73074c1
ayushmaannsingh <sa776857@gmail.com>
2026-02-13 20:35:05 +0530

    Functions

--- Functions/Practice.py
+++ Functions/Practice.py
@@ -19,3 +19,34 @@ def print_list(list):
         print(item, end=" ")
     
 print_list(heroes)
+
+
+# WAP to find the factorial of n. 
+  
+def cal_fact(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact *= i
+    print(fact)
+
+cal_fact(5)
+
+# WAP to convert USD to INR
+
+def converter(usd_val):
+    inr_val = usd_val * 90
+    print(usd_val, "USD =", inr_val, "INR")
+
+converter(100)
+
+# WAP to print EVEN or ODD number.
+
+n = int(input("enter your num : "))
+
+def odd_or_even(n):
+    if(n%2==0):
+        print("EVEN")
+    else:
+        print("ODD")
+
+odd_or_even(n)

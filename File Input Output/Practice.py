cc24fa978cf40d0bee558730359cdfa364b05d8a
ayushmaannsingh <sa776857@gmail.com>
2026-02-14 22:50:34 +0530

    Practice ques

---
+++ File Input Output/Practice.py
@@ -0,0 +1,6 @@
+# Create a new file "practice.txt" using python. Add the folling data in it:
+
+with open("practice.txt", "w") as f:
+    f.write("Hi everyone\nI am learning File I/O ")
+    f.write("using python.\nI like programming in python.")
+ 

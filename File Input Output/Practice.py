fc328d6c613a4c79c1f7e0ae47fe626bf0985cb5
ayushmaannsingh <sa776857@gmail.com>
2026-02-14 23:16:24 +0530

    Practice Ques

--- File Input Output/Practice.py
+++ File Input Output/Practice.py
@@ -6,11 +6,25 @@
 
 # WAF that replace all occurrences of "Python" with "Java" in above file.
 
-with open("practice.txt", "r") as f:
-    data = f.read()
+# with open("practice.txt", "r") as f:
+#     data = f.read()
 
-new_data = data.replace("python", "java")
-print(new_data)
+# new_data = data.replace("python", "java")
+# print(new_data)
 
-with open("practice.txt", "w") as f:
-    f.write(new_data)
+# with open("practice.txt", "w") as f:
+#     f.write(new_data)
+
+
+# Search if the word "learning" exists in the file or not.
+
+def check_for_word():
+    word = "learning"
+    with open("practice.txt", "r") as f:
+        data = f.read()
+        if(data.find(word) !=-1):
+            print("Found")
+        else:
+            print("not found")
+
+check_for_word()

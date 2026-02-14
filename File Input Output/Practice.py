3fd2adadb6236bf546727613155ee441ccbfe0fd
ayushmaannsingh <sa776857@gmail.com>
2026-02-14 23:32:02 +0530

    Practice ques

--- File Input Output/Practice.py
+++ File Input Output/Practice.py
@@ -18,13 +18,33 @@
 
 # Search if the word "learning" exists in the file or not.
 
-def check_for_word():
-    word = "learning"
-    with open("practice.txt", "r") as f:
-        data = f.read()
-        if(data.find(word) !=-1):
-            print("Found")
-        else:
-            print("not found")
+# def check_for_word():
+#     word = "learning"
+#     with open("practice.txt", "r") as f:
+#         data = f.read()
+#         if(data.find(word) !=-1):
+#             print("Found")
+#         else:
+#             print("not found")
 
-check_for_word()
+# check_for_word()
+
+
+# WAF to find in which line of the file does the word "learning" occur first.
+# Print -1 if word not found
+
+def check_for_line():
+    word = "like"
+    data = True
+    line_no = 1
+    with open("practice.txt", "r") as f:
+        while data:
+            data = f.readline()
+            if (word in data):
+                print(line_no)
+                return
+            line_no +=1
+
+    return -1
+
+print(check_for_line)

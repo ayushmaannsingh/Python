6977d980cefbf56926c9c48a58d1773928b75413
ayushmaannsingh <sa776857@gmail.com>
2026-02-16 23:02:22 +0530

    OOPS

--- OOPS/OOPS.py
+++ OOPS/OOPS.py
@@ -4,12 +4,23 @@
 # s1 = Student()      # Object
 # print(s1.name)
 
-class Car:
-     color = "blue"
-     brand = "mercedes"
+# class Car:
+#      color = "blue"
+#      brand = "mercedes"
     
-car1 = Car()
-print(car1.color)
-print(car1.brand)
+# car1 = Car()
+# print(car1.color)
+# print(car1.brand)
+
+class Student:
+    def __init__(self, fullname):
+        self.name = fullname
+        print("adding new student in database")
+
+s1 = Student("Ayush")
+print(s1.name)
+
+s2 = Student("Akash")
+print(s2.name)
 
 

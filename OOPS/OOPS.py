1f03dffd933f300e22e92d0207a07a1182d20528
ayushmaannsingh <sa776857@gmail.com>
2026-02-16 21:54:56 +0530

    Opps

--- OOPS/OOPS.py
+++ OOPS/OOPS.py
@@ -1,5 +1,15 @@
-class Student:
-    name = "Ayush"
+# class Student:       # Class
+#     name = "Ayush"
 
-s1 = Student()
-print(s1.name)
+# s1 = Student()      # Object
+# print(s1.name)
+
+class Car:
+     color = "blue"
+     brand = "mercedes"
+    
+car1 = Car()
+print(car1.color)
+print(car1.brand)
+
+

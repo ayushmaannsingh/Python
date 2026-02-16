eb25a84fb9e26b3f862f56531adf04ab493b9ed2
ayushmaannsingh <sa776857@gmail.com>
2026-02-17 01:55:02 +0530

    oops

---
+++ OOPS/practiceQues.py
@@ -0,0 +1,21 @@
+# Create student class that takes name and marks of 3 subjects as arguments in constructor.
+# Then create a method to print the average.
+
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    @staticmethod
+    def hello():
+        print("Decorator")
+
+    def get_avg(self):
+        sum = 0
+        for val in self.marks:
+            sum += val
+        print("hi", self.name, "your avg score is:", sum/3)
+
+s1 = Student("tony stark", [99, 98, 97])
+s1.get_avg()
+s1.hello()

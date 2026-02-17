fbad79a6784b158e91932189de4f649c10630769
ayushmaannsingh <sa776857@gmail.com>
2026-02-18 01:05:41 +0530

    OOPS

--- OOPS/Inheritance.py
+++ OOPS/Inheritance.py
@@ -20,3 +20,26 @@ car2 = ToyotaCar("prius")
 
 print(car1.name)
 print(car1.color)
+
+# Multi-level Inheritance
+
+class Car_Car:
+    
+    @staticmethod
+    def start():
+        print("car started..")
+
+    @staticmethod
+    def stop():
+        print("car stopped.")
+
+class ToyotaCar(Car_Car):
+    def __init__(self, brand):
+        self.name = brand
+
+class Fortuner(ToyotaCar):
+    def __init__(self, type):
+        self.type = type
+
+car1 = Fortuner("disel")
+car1.start()

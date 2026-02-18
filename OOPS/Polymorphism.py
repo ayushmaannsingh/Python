a96a24b0fcf4f02a648fef9ce84cd7f5d044ac0b
ayushmaannsingh <sa776857@gmail.com>
2026-02-19 00:31:00 +0530

    polymorphism

--- OOPS/Polymorphism.py
+++ OOPS/Polymorphism.py
@@ -20,11 +20,16 @@ class Complex:
         newImg = self.img + num2.img
         return Complex(newReal, newImg)
 
+
+    def __sub__(self, num2):
+        newReal = self.real - num2.real
+        newImg = self.img - num2.img
+        return Complex(newReal, newImg)
 num1 = Complex(1, 3)
 num1.showNumber()
 
 num2 = Complex(4, 6)
 num2.showNumber()
 
-num3 = num1 + num2
+num3 = num1 - num2
 num3.showNumber()
